1e8df7ef65687b92b577cf166ea12717cac45e6d
Adidharma Torutama <adidharmatoru@gmail.com>
2025-05-07 01:45:15 +0900

    fix websocket ☁️

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,12 +15,12 @@ export default defineNuxtConfig({
     livekit: {
       apiKey: process.env.LIVEKIT_API_KEY,
       apiSecret: process.env.LIVEKIT_API_SECRET,
-      wsUrl: process.env.LIVEKIT_WS_URL
+      wsUrl: 'wss://ws.adidharmatoru.dev'
     },
     // Keys within public are exposed to the client
     public: {
       livekit: {
-        wsUrl: process.env.LIVEKIT_WS_URL
+        wsUrl: 'wss://ws.adidharmatoru.dev'
       }
     }
   },
